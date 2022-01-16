68dc6eb7008eda6fa901ca3dd4c17c50ed22ea41
plvhx <gandung@slackerc0de.us>
2022-01-16 21:10:15 +0700

    .Refactoring app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import jsonify
 from flask import request
 
 app = Flask(__name__)
@@ -10,4 +11,20 @@ def hello_world():
 @app.route('/callback', methods=['POST'])
 def agent_callback():
 	open('/tmp/req', 'wb').write(bytes(request.data.encode('utf-8')))
-	return 'Will be processed.'
+	return jsonify({
+		'data': {
+			'agent': {
+				'id': 1776,
+				'name': 'Bill Tanthowi Jauhari',
+				'sdk_email': 'Ny83g_bill@gmail.com',
+				'email': 'bill@gmail.com',
+				'sdk_key': '2xxxxxxxxxxxea743',
+				'type': 2,
+				'is_available': false,
+				'avatar_uri': null,
+				'is_verified': false,
+				'force_offline': false,
+				'count': 0
+			}
+		}
+	})
